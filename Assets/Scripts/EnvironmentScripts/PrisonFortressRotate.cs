a269be4629f2dba3e823520df47c935e17032e7e
Rowan Donaldson <nerdrowan@gmail.com>
2015-08-05 19:20:12 +1000

    Added: PrisonFortressPrefab Changed: File structure for Colour Materials

---
+++ Assets/Scripts/EnvironmentScripts/PrisonFortressRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Rigidbody))]
+public class PrisonFortressRotate : MonoBehaviour 	//This script rotates the Prison Fortress ship in local space
+{
+	private Rigidbody myRigid;
+	public float rotateForce = 10.0f;
+
+	void Start () 
+	{
+		myRigid = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () 
+	{
+		myRigid.AddRelativeTorque(Vector3.up * rotateForce);
+	}
+}
