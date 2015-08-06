4e73367703e3eaa4733f7bd4346c418903707308
Pat Ferguson <patrick.ferguson@ancientdawngames.com>
2015-08-06 17:01:19 +1000

    Changed: Made the roulette wheel slerp to its final position instead of on whatever angle it finishes on.

--- Assets/Scripts/PlayerAirship/Effects & Features/RouletteSpinWheel.cs
+++ Assets/Scripts/PlayerAirship/Effects & Features/RouletteSpinWheel.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿/**
+ * File: RouletteSpinWheel.cs
+ * Author: Rowan Donaldson
+ * Maintainer: Patrick Ferguson
+ * Created: 6/08/2015
+ * Copyright: (c) 2015 Team Storms, All Rights Reserved.
+ * Description: Gives the player
+ **/
+
+using UnityEngine;
 using System.Collections;
 
 public class RouletteSpinWheel : MonoBehaviour // This script manages the 'Spinning Wheel' effect on the Roulette Wheel
 												// Inputs are passed in from Input Manager script VIA the Roulette Behaviour State script.
 {
-	private Rigidbody m_myRigid;
 	private float changeAngularDrag;
 	
 	public GameObject rotatorJoint;
@@ -13,56 +21,76 @@ public class RouletteSpinWheel : MonoBehaviour // This script manages the 'Spinn
 	
 	private bool inputStop;
 	private bool inputSpeedUp;
-	
 
-	void OnEnable () 
+    /// <summary>
+    /// For making the roulette wheel's spin finish on a 90 degree boundary. 
+    /// </summary>
+    private Quaternion targetFinalRotation = Quaternion.identity;
+
+    // Cached variables
+    private Rigidbody m_myRigid;
+    private Transform m_trans;
+    private Transform m_rotatorTrans;
+
+    void Start()
+    {
+        m_rotatorTrans = rotatorJoint.transform;
+        m_trans = transform;
+    }
+
+	void OnEnable() 
 	{
 
-		//Reset Position
+		// Reset Position
 		gameObject.transform.localPosition = Vector3.zero;
 		gameObject.transform.localRotation = Quaternion.Euler(Vector3.zero);
-		gameObject.transform.localScale = new Vector3(4, 1, 1);	//try not to change these
+		gameObject.transform.localScale = new Vector3(4, 1, 1);	// Try not to change these
 	
-		//Set/Reset physics components
+		// Set/Reset physics components
 		m_myRigid = gameObject.GetComponent<Rigidbody>();
-		//Set Components of the rigidbody in the start.
+		// Set Components of the rigidbody in the start.
 		
 		m_myRigid.isKinematic = false;
 		m_myRigid.useGravity = false;
 		
 		m_myRigid.drag = 0;
-		m_myRigid.angularDrag = 0.001f;	//low on start
+		m_myRigid.angularDrag = 0.001f;	// Low on start
+
+        m_myRigid.maxAngularVelocity = 14;
+
+        targetFinalRotation = Quaternion.identity;
 		
-		m_myRigid.maxAngularVelocity = 14;	//default 7
-		
-		//get the startRotation of the handle joint
+		// Get the start rotation of the handle joint
 		rotateAmount = 0;
 		
 		//Make the wheel spin
 		Spin();
 	}
 	
-	void Update () 
+	void Update() 
 	{
-		if (inputSpeedUp)	// For testing- make the roulette wheel spin faster
+        // For testing- make the roulette wheel spin faster
+		if (inputSpeedUp)
 		{
 			Spin();
 		}
-		
-		if (inputStop)		// Slow the roulette wheel by increasing the rigidbody Drag
+
+        // Slow the roulette wheel by increasing the rigidbody drag
+		if (inputStop)
 		{
 			m_myRigid.angularDrag = Mathf.Lerp(m_myRigid.angularDrag, 1, 0.25f);	
 			
 			pullHandle = true;
 		}
-		
-		
-		if (pullHandle)	//Handle effect
+
+        //Handle effect
+		if (pullHandle)
 		{
 			rotateAmount = Mathf.Lerp(rotateAmount, -90, Time.deltaTime * 5.0f);
 		}
 		
-		if (rotatorJoint.transform.localEulerAngles.x == 270)	// Make handle return to original position
+        // Make handle return to original position
+		if (m_rotatorTrans.localEulerAngles.x == 270)
 		{
 			pullHandle = false;
 		}
@@ -73,22 +101,36 @@ public class RouletteSpinWheel : MonoBehaviour // This script manages the 'Spinn
 			rotateAmount = Mathf.Lerp(rotateAmount, 0, Time.deltaTime * 10.0f);
 		}
 		
-		//Lock the rotation of the handle to One Axis
-		float rotationY = rotatorJoint.transform.localEulerAngles.y;
-		float rotationZ = rotatorJoint.transform.localEulerAngles.z;
-		//move the joint
-		rotatorJoint.transform.localRotation = Quaternion.Euler(rotateAmount, rotationY, rotationZ);
-		
-		// Use this space to check the Result of the roulettewheel
-		
+		// Lock the rotation of the handle to One Axis
+		float rotationY = m_rotatorTrans.localEulerAngles.y;
+		float rotationZ = m_rotatorTrans.localEulerAngles.z;
+		// Move the joint
+		m_rotatorTrans.localRotation = Quaternion.Euler(rotateAmount, rotationY, rotationZ);
+
+        // Slerp into the final position if applicable
+        if (targetFinalRotation != Quaternion.identity)
+        {
+            m_trans.localRotation = Quaternion.Slerp(m_trans.localRotation, targetFinalRotation, Time.deltaTime * 5.0f);
+        }
+
 		//NOTE: Right now- this just triggers the State Manager to change the object from ROULETE to NORMAL CONTROL
 		if (m_myRigid.angularVelocity.x > -1.0f && m_myRigid.angularVelocity.x < 0)
-		{
-			gameObject.transform.parent.transform.GetComponentInParent<StateManager>().currentPlayerState = EPlayerState.Control;
+        {
+            int selectedIndex = Mathf.RoundToInt(m_myRigid.rotation.eulerAngles.x / 90.0f);
+            if (Mathf.RoundToInt(m_myRigid.rotation.eulerAngles.x % 90.0f) == 0)
+            {
+                targetFinalRotation = Quaternion.identity;
+                Debug.Log("DONE!");
+                //RouletteDone(selectedIndex);
+            }
+            else
+            {
+                // Not aligned to the final spin down position, slerp to it
+                targetFinalRotation = Quaternion.AngleAxis(selectedIndex * 90.0f, new Vector3(1, 0, 0));
+            }
 		}
 		
 	}
-	
 	
 	void OnDisable()
 	{
@@ -106,4 +148,16 @@ public class RouletteSpinWheel : MonoBehaviour // This script manages the 'Spinn
 		m_myRigid.angularDrag = 0.001f;
 		m_myRigid.AddRelativeTorque(Vector3.left * 100, ForceMode.Impulse);
 	}
+
+    /// <summary>
+    /// Called when the roulette wheel finishes spinning.
+    /// </summary>
+    /// <param name="a_finalSelectionIndex">Index of the roulette wheel once the spin has finished.</param>
+    void RouletteDone(int a_finalSelectionIndex)
+    {
+        // TODO Apply roulette reward buff here
+
+        // Switch to the gameplay state
+        gameObject.transform.parent.transform.GetComponentInParent<StateManager>().currentPlayerState = EPlayerState.Control;
+    }
 }
