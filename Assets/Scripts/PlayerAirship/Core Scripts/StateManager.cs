de575bd9136dd28c0ee60acf984007a23595aa1e
Pat Ferguson <patrick.ferguson@scann3d.com.au>
2015-08-17 00:11:01 +1000

    Changed: Made the press any button/axis function in the pre-game wait have a deadzone on stick input.

--- Assets/Scripts/PlayerAirship/Core Scripts/StateManager.cs
+++ Assets/Scripts/PlayerAirship/Core Scripts/StateManager.cs
@@ -201,21 +201,21 @@ public class StateManager : MonoBehaviour
         string cntPrefix = gameObject.tag;
 
         // Check if any button is down
-        if (Input.GetButton(cntPrefix + "Start")        ||
-            Input.GetButton(cntPrefix + "Select")       || 
-            Input.GetButton(cntPrefix + "FaceDown")     ||
-            Input.GetButton(cntPrefix + "FaceUp")       ||
-            Input.GetButton(cntPrefix + "FaceLeft")     ||
-            Input.GetButton(cntPrefix + "FaceRight")    || 
-            Input.GetButton(cntPrefix + "BumperLeft")   ||
-            Input.GetButton(cntPrefix + "BumperRight")  ||
-            Input.GetButton(cntPrefix + "Triggers")     ||
-            Input.GetButton(cntPrefix + "ClickLeft")    ||
-            Input.GetButton(cntPrefix + "ClickRight")   ||
-            Input.GetAxisRaw(cntPrefix + "Horizontal")      != 0 ||
-            Input.GetAxisRaw(cntPrefix + "Vertical")        != 0 ||
-            Input.GetAxisRaw(cntPrefix + "CamHorizontal")   != 0 ||
-            Input.GetAxisRaw(cntPrefix + "CamVertical")     != 0)
+        if (Input.GetButton(cntPrefix + "Start")       ||
+            Input.GetButton(cntPrefix + "Select")      || 
+            Input.GetButton(cntPrefix + "FaceDown")    ||
+            Input.GetButton(cntPrefix + "FaceUp")      ||
+            Input.GetButton(cntPrefix + "FaceLeft")    ||
+            Input.GetButton(cntPrefix + "FaceRight")   || 
+            Input.GetButton(cntPrefix + "BumperLeft")  ||
+            Input.GetButton(cntPrefix + "BumperRight") ||
+            Input.GetButton(cntPrefix + "ClickLeft")   ||
+            Input.GetButton(cntPrefix + "ClickRight")  ||
+            Mathf.Abs(Input.GetAxisRaw(cntPrefix + "Triggers"))      >= 0.25f ||
+            Mathf.Abs(Input.GetAxisRaw(cntPrefix + "Horizontal"))    >= 0.25f ||
+            Mathf.Abs(Input.GetAxisRaw(cntPrefix + "Vertical"))      >= 0.25f ||
+            Mathf.Abs(Input.GetAxisRaw(cntPrefix + "CamHorizontal")) >= 0.25f ||
+            Mathf.Abs(Input.GetAxisRaw(cntPrefix + "CamVertical"))   >= 0.25f)
         {
             return true;
         }
