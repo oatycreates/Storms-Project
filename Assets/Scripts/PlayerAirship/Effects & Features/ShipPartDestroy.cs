220d9efd0414c66c5b9ee44a39a7bcf50db922d2
Pat Ferguson <patrick.ferguson@ancientdawngames.com>
2015-08-20 17:28:39 +1000

    Changed: Began work on the ship part destruction.
    TODO: Change the mass of the ship on part destroy.

---
+++ Assets/Scripts/PlayerAirship/Effects & Features/ShipPartDestroy.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * File: ShipPartDestroy.cs
+ * Author: Patrick Ferguson
+ * Created: 20/08/2015
+ * Copyright: (c) 2015 Team Storms, All Rights Reserved.
+ * Description: Manages the destruction of player ship parts, and parent mass change.
+ **/
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Manages the destruction of player ship parts, and parent mass change.
+/// </summary>
+public class ShipPartDestroy : MonoBehaviour
+{
+    // Cached variables
+    private Rigidbody m_shipRB;
+
+    void Awake()
+    {
+        //m_shipRB = ;
+    }
+
+	/// <summary>
+    /// Use this for initialisation.
+	/// </summary>
+	void Start()
+    {
+	
+	}
+	
+	/// <summary>
+    /// Update is called once per frame.
+	/// </summary>
+	void Update()
+    {
+	
+	}
+}
