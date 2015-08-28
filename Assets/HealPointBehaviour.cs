fc54cc43ba00518fcb81404cd18ec1e153ecc315
Rowan Donaldson <nerdrowan@gmail.com>
2015-08-28 12:01:31 +1000

    Added: A temp respawn limb prefab added.

---
+++ Assets/HealPointBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealPointBehaviour : MonoBehaviour 
+{
+
+	void OnTriggerEnter(Collider other)
+	{
+		if ((other.gameObject.tag == "Player1_")||(other.gameObject.tag == "Player2_")||(other.gameObject.tag == "Player3_")||(other.gameObject.tag == "Player4_"))
+		{
+			if(other.transform.root.gameObject.GetComponent<ShipPartDestroy>() != null)
+			{
+				other.transform.root.gameObject.GetComponent<ShipPartDestroy>().RepairAllParts();
+			}
+
+		}
+	}
+}
