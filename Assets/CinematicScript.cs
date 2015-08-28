f957aede576b43eadd7d0cc715ad5ee7a7d5557e
Rowan Donaldson <nerdrowan@gmail.com>
2015-08-28 17:20:55 +1000

    Added: Made changes to RoDoScene and added Cinematic Script to experiment with.

---
+++ Assets/CinematicScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CinematicScript : MonoBehaviour 
+{
+	public float twirlSpeed = 20;
+
+	public GameObject upDownTarget;
+	private float upDown;
+
+	//Need to make it work in local space
+
+	void Update () 
+	{
+		gameObject.transform.Rotate (Vector3.up * twirlSpeed * Time.deltaTime);
+
+		upDown = Mathf.PingPong (Time.time, -3);
+
+		upDownTarget.transform.position = new Vector3 (upDownTarget.transform.position.x, upDown, upDownTarget.transform.position.z);
+
+	}
+}
