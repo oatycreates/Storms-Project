b63adaf3ceb6c862cee4063aa6222f8ad72c37f0
Rowan Donaldson <nerdrowan@gmail.com>
2015-09-02 12:31:31 +1000

    Changed: Minor changes to RoDo test scene. Added: Script and prefab for Outro Cinematic

---
+++ Assets/CinematicOutro.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * File: CinematicScript.cs
+ * Author: Rowan Donaldson
+ * Maintainer: Patrick Ferguson
+ * Created: 02/09/2015
+ * Copyright: (c) 2015 Team Storms, All Rights Reserved.
+ * Description: This script controls the 'Cinematic' and camera that is triggered at the end of a gameplay match.
+ **/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CinematicOutro : MonoBehaviour 
+{
+	public GameObject rotator;
+	public GameObject pirate;
+	private Renderer pirateRenderer;
+	public Text winText;
+	public Color myColour = Color.blue;
+
+
+	void Awake () 
+	{
+		pirateRenderer = pirate.GetComponent<Renderer> ();
+
+		//Go to sleep untill I'm needed
+		gameObject.SetActive (false);
+	}
+
+	void FixedUpdate () 
+	{
+		rotator.transform.Rotate(Vector3.down * Time.deltaTime * 60);
+
+		pirateRenderer.material.color = myColour;
+		winText.color = myColour;
+		winText.text = "HELLO WORLD";
+	}
+
+}
