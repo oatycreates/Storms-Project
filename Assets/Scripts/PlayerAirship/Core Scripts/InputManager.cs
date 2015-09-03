882d4990ee6929278312cc56b3a6f1efa20e6c11
Andrew Barbour <andrew.barbour1@gmail.com>
2015-09-03 11:25:53 +1000

    Changed: Boost can now be stopped by pressing back/select while boosting

--- Assets/Scripts/PlayerAirship/Core Scripts/InputManager.cs
+++ Assets/Scripts/PlayerAirship/Core Scripts/InputManager.cs
@@ -136,7 +136,7 @@ namespace ProjectStorms
             bool faceUp = Input.GetButton(gameObject.tag + "FaceUp");
 
             // Start and Select
-            //bool select = Input.GetButton(gameObject.tag + "Select");
+            bool select = Input.GetButton(gameObject.tag + "Select");
             //bool start = Input.GetButton(gameObject.tag + "Start");
 
             // Analogue Stick Clicks
@@ -147,7 +147,7 @@ namespace ProjectStorms
             // Send variable data to individual scripts
             m_rouletteControl.PlayerInput(faceDown, faceUp);	// Use the face button inputs to Stop/Start the roulette wheel
             m_standardControl.PlayerInputs(upDown, leftRight, camUpDown, camLeftRight, triggers, bumperLeft, bumperRight, faceUp, faceDown, faceLeft, faceRight);
-            m_fireshipControl.PlayerFireshipInputs(upDown, leftRight);
+            m_fireshipControl.PlayerFireshipInputs(upDown, leftRight, select);
             m_rotateCam.PlayerInputs(camUpDown, camLeftRight, triggers, faceDown, bumperLeft, bumperRight, clickLeft, clickRight);
             m_shuntingControl.PlayerInputs(bumperLeft, bumperRight);
         }
