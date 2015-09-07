5885751893e2bd8d2e7caf2b612e6adc28c420cc
Rowan Donaldson <nerdrowan@gmail.com>
2015-09-07 23:51:50 +1000

    Changed: Minor changes to RoDoTestScene, Skywhale Script and gamescene.

--- Assets/Scripts/SkyWhaleFlight.cs
+++ Assets/Scripts/SkyWhaleFlight.cs
@@ -16,28 +16,129 @@ namespace ProjectStorms
 	[RequireComponent(typeof(Rigidbody))]
 	public class SkyWhaleFlight : MonoBehaviour 
 	{
+		
 		private Rigidbody myRigid;
-		public GameObject whaleTarget;
-
-		public Vector3 eulerAngularVelocity;
-
-		void Awake () 
+		
+		public GameObject followObject;
+		
+		public float turnSpeed = 1;
+		public float moveSpeed = 12;
+		
+		public bool speedByDistance = false;
+		
+		private Quaternion lookRotation;
+		private Vector3 direction;
+		
+		private float distanceToTarget;
+		
+		private GameObject closest = null;
+		
+		//Stuff for the editor
+		private Vector3 tempDirection;
+		private float tempDistance;
+		
+		void Awake()
 		{
-			myRigid = gameObject.GetComponent<Rigidbody> ();
+			myRigid = gameObject.GetComponent<Rigidbody>();
 		}
-
+		
+		void Start()
+		{
+			//Start by spawning a target;
+			SpawnATarget();
+			
+			//Spawn a Target Every Second
+			InvokeRepeating("SpawnATarget", 0, 1);
+		}
+		
 		void FixedUpdate () 
 		{
-			//myRigid.MovePosition (myRigid.transform.position + transform.forward * Time.deltaTime * 20);
-
-
-			//myRigid.AddRelativeForce (Vector3.forward * 5);
-
-			//myRigid.AddRelativeTorque (Vector3.forward * 1);
-
-			Quaternion deltaRotation = Quaternion.Euler (eulerAngularVelocity * Time.fixedDeltaTime);
-
-			myRigid.MoveRotation (myRigid.rotation * deltaRotation);
+			// I think this function needs to be called regularly
+			FindClosestNode();
+			
+			Rotating();
+			Moving();
+			
+			Debug.DrawRay(myRigid.transform.position, direction * distanceToTarget, Color.green);
 		}
+		
+		//Hmm this is the example from the Unity Scripting API page
+		GameObject FindClosestNode()
+		{
+			GameObject[] gos;		
+			gos = GameObject.FindGameObjectsWithTag("AINode");
+			
+			//GameObject closest = null;	
+			float distance = Mathf.Infinity;	
+			Vector3 position = myRigid.transform.position;
+			
+			foreach (GameObject go in gos)
+			{
+				Vector3 diff = go.transform.position - position;
+				float curDistance = diff.sqrMagnitude;
+				
+				if (curDistance < distance)
+				{
+					closest = go;
+					distance = curDistance;
+				}
+			}		
+			return closest;			
+		}
+		
+		
+		
+		void Rotating()
+		{		
+			//Direciton between me and the other object
+			direction = (closest.transform.position - myRigid.transform.position).normalized;
+			
+			//Create the rotation to the target
+			lookRotation = Quaternion.LookRotation(direction);
+			
+			//Rotate over time
+			//gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, lookRotation, turnSpeed * Time.deltaTime);
+			Quaternion tempTurn = Quaternion.Slerp(myRigid.transform.rotation, lookRotation, turnSpeed * Time.deltaTime);
+			
+			myRigid.MoveRotation(tempTurn);
+		}
+		
+		
+		void Moving()
+		{
+			//Distance to target object
+			distanceToTarget = Vector3.Distance(closest.transform.position, myRigid.transform.position);
+			
+			float tempDistanceValue = distanceToTarget;
+			//Clamp the distance value, so we can then use it in the Movement vector
+			Mathf.Clamp(tempDistanceValue, 0, 1);
+			
+			Vector3 tempPos;
+			
+			if (speedByDistance)
+			{
+				//Move over time
+				tempPos = (myRigid.transform.position + myRigid.transform.forward * Time.deltaTime * tempDistanceValue);
+			}
+			else
+			{
+				//Move with standard speed
+				tempPos = (myRigid.transform.position + myRigid.transform.forward * Time.deltaTime * moveSpeed);
+			}
+			
+			myRigid.MovePosition(tempPos);
+		}
+		
+		
+		void SpawnATarget()
+		{
+			GameObject tempTarget = new GameObject("WhaleNode");
+			tempTarget.transform.position = followObject.transform.position;
+			tempTarget.transform.rotation = followObject.transform.rotation;
+			tempTarget.tag = "AINode";
+			Destroy(tempTarget, 1.5f);
+			
+		}
+		
 	}
 }
