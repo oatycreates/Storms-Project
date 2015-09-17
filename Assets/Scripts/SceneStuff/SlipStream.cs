91f4eaa5c28ba100b928e6d61d42ed9df6b41170
Pat Ferguson <patrick.ferguson@ancientdawngames.com>
2015-09-17 15:40:29 +1000

    Changed: Fixed SlipStream player finding bug.

--- Assets/Scripts/SceneStuff/SlipStream.cs
+++ Assets/Scripts/SceneStuff/SlipStream.cs
@@ -18,37 +18,39 @@ namespace ProjectStorms
     {
         public float force = 100.0f;
 
-        List<Rigidbody> m_playerRigidBodies = new List<Rigidbody>(4);
+        Rigidbody[] m_playerRigidBodies = new Rigidbody[4];
 
         void GetPlayerRigidBodies()
         {
-            Rigidbody body = null;
-
             // Airships are the only things that will have AirshipControlBehaviours, this will only change if we refactor the input system
             AirshipControlBehaviour[] objs = GameObject.FindObjectsOfType<AirshipControlBehaviour>();
-            foreach (AirshipControlBehaviour script in objs)
+            // Use the script to find the rigidbody
+            StoreBodyInSlot(objs[0].GetComponent<Rigidbody>());
+            StoreBodyInSlot(objs[1].GetComponent<Rigidbody>());
+            StoreBodyInSlot(objs[2].GetComponent<Rigidbody>());
+            StoreBodyInSlot(objs[3].GetComponent<Rigidbody>());
+        }
+
+        private void StoreBodyInSlot(Rigidbody a_body)
+        {
+            // Store in the slot
+            if (a_body != null)
             {
-                // Use the script to find the rigidbody.
-                body = script.GetComponent<Rigidbody>();
-                
-                if (body != null)
+                if (a_body.CompareTag("Player1_"))
                 {
-                    if (body.CompareTag("Player1_"))
-                    {
-                        m_playerRigidBodies[0] = body;
-                    }
-                    else if (body.CompareTag("Player2_"))
-                    {
-                        m_playerRigidBodies[1] = body;
-                    }
-                    else if (body.CompareTag("Player3_"))
-                    {
-                        m_playerRigidBodies[2] = body;
-                    }
-                    else if (body.CompareTag("Player4_"))
-                    {
-                        m_playerRigidBodies[3] = body;
-                    }
+                    m_playerRigidBodies[0] = a_body;
+                }
+                else if (a_body.CompareTag("Player2_"))
+                {
+                    m_playerRigidBodies[1] = a_body;
+                }
+                else if (a_body.CompareTag("Player3_"))
+                {
+                    m_playerRigidBodies[2] = a_body;
+                }
+                else if (a_body.CompareTag("Player4_"))
+                {
+                    m_playerRigidBodies[3] = a_body;
                 }
             }
         }
@@ -63,38 +65,41 @@ namespace ProjectStorms
         {
             Rigidbody playerBody = null;
 
-            switch (a_other.tag)
+            if (m_playerRigidBodies.Length > 0)
             {
-                case "Player1_":
-                    playerBody = m_playerRigidBodies[0];
-                    break;
+                switch (a_other.tag)
+                {
+                    case "Player1_":
+                        playerBody = m_playerRigidBodies[0];
+                        break;
 
-                case "Player2_":
-                    playerBody = m_playerRigidBodies[1];
-                    break;
+                    case "Player2_":
+                        playerBody = m_playerRigidBodies[1];
+                        break;
 
-                case "Player3_":
-                    playerBody = m_playerRigidBodies[2];
-                    break;
+                    case "Player3_":
+                        playerBody = m_playerRigidBodies[2];
+                        break;
 
-                case "Player4_":
-                    playerBody = m_playerRigidBodies[3];
-                    break;
+                    case "Player4_":
+                        playerBody = m_playerRigidBodies[3];
+                        break;
 
-                case "Passengers":
-                    playerBody = a_other.GetComponentInParent<Rigidbody>();
-                    break;
+                    case "Passengers":
+                        playerBody = a_other.GetComponentInParent<Rigidbody>();
+                        break;
 
-                default:
-                    // Not player
-                    playerBody = a_other.GetComponent<Rigidbody>();
-                    return;
-            }
+                    default:
+                        // Not player
+                        playerBody = a_other.GetComponent<Rigidbody>();
+                        return;
+                }
 
-            // Ensure that player has a rigidbody
-            if (playerBody != null)
-            {
-                playerBody.AddForce(transform.forward, ForceMode.VelocityChange);
+                // Ensure that player has a rigidbody
+                if (playerBody != null)
+                {
+                    playerBody.AddForce(transform.forward, ForceMode.VelocityChange);
+                }
             }
         }
     }
