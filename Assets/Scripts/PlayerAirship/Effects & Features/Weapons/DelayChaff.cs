4ea281d69cd347916052c3c7bd8c8a0d35b88c7a
Pat Ferguson <patrick.ferguson@ancientdawngames.com>
2015-09-24 16:44:53 +1000

    Changed: Added the beginnings of the d-pad chaff weapon.

---
+++ Assets/Scripts/PlayerAirship/Effects & Features/Weapons/DelayChaff.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * File: DelayChaff.cs
+ * Author: Patrick Ferguson
+ * Maintainers: 
+ * Created: 24/09/2015
+ * Copyright: (c) 2015 Team Storms, All Rights Reserved.
+ * Description: Dpad slowing field weapon.
+ **/
+
+using UnityEngine;
+using System.Collections;
+
+namespace ProjectStorms
+{
+	public class DelayChaff : MonoBehaviour 
+	{
+		void Start() 
+		{
+		
+		}
+		
+		void Update() 
+		{
+		
+		}
+	}
+}
