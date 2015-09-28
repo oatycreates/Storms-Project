37f4816d6cfa165e34a5183329ea58425a84ab0a
Rowan Donaldson <nerdrowan@gmail.com>
2015-09-28 23:08:25 +1000

    Changed: Made changes to the Missile Flight script. Missiles can now be dodged by shunting to the side at the last second.

--- Assets/MissileFlight.cs
+++ Assets/MissileFlight.cs
@@ -16,6 +16,7 @@ namespace ProjectStorms
 	public class MissileFlight : MonoBehaviour 
 	{
 		public GameObject target;
+
 		private Rigidbody myRigid;
 
 		private bool moving = false;
@@ -23,13 +24,21 @@ namespace ProjectStorms
 		//public float jumpForce = 30;
 		public float movementVelocity = 1;
 		public float turnSpeed = 5;
-		public float spiralSpeed = 10;
+		//public float spiralSpeed = 10;
 
-		private Vector3 startPoint;
+		private GameObject targetProxy;
+		//public float retargetSpeed = 2;
+
+		public float closeRangeThreshold = 20;
+		private Color status = Color.white;
+		public GameObject childRenderer;
 
 		void Awake()
 		{
 			myRigid = gameObject.GetComponent<Rigidbody> ();
+
+			targetProxy = new GameObject();
+			targetProxy.name = "MissileTarget";
 		}
 
 		void Start () 
@@ -44,37 +53,56 @@ namespace ProjectStorms
 			{
 				target = GameObject.FindGameObjectWithTag("Player1_");
 				//Fire();
-				print("World Centre");
+				print("Found Target");
 			}
-
-			startPoint = gameObject.transform.position;
 		}
 
 		void Update () 
 		{
-			if (Application.isEditor) //Quick Reset
-			{
+			//if (Application.isEditor) //Quick Reset
+			//{
 				if (Input.GetKeyDown(KeyCode.Space))
 				{
-					//gameObject.transform.position = startPoint;
 					Fire();
 				}
+			//}
+
+
+			//Raycast
+			Vector3 rayDirection = (targetProxy.transform.position - gameObject.transform.position).normalized;
+			float rayDistance = Vector3.Distance (targetProxy.transform.position, gameObject.transform.position);
+
+			if (moving)
+			{
+				Debug.DrawRay (gameObject.transform.position, rayDirection * rayDistance, Color.red);
 			}
+			else
+			if (!moving)
+			{
+				Debug.DrawRay(gameObject.transform.position, rayDirection * rayDistance, Color.green);
+			}
+
+			//Set colours
+			if (childRenderer != null)
+			{
+				childRenderer.GetComponent<MeshRenderer>().material.color = status;
+			}
+			else
+			{
+				print("No child object attached.");
+			}
+
 		}
 
 		void FixedUpdate()
 		{
-			if (moving)
-			{
+			if (moving) {
 				myRigid.velocity = transform.forward * movementVelocity;
 
+				/*
 				if (target != null)
 				{
 					Quaternion targetDirection = Quaternion.LookRotation(target.transform.position - myRigid.transform.position);
-
-					//Quaternion rotationDirection = Quaternion.AngleAxis(spiralSpeed * Time.time, Vector3.forward);
-
-					//targetDirection *= rotationDirection;
 
 					myRigid.MoveRotation(Quaternion.RotateTowards(myRigid.transform.rotation, targetDirection, turnSpeed));
 				}
@@ -84,30 +112,86 @@ namespace ProjectStorms
 					Quaternion zeroDirection = Quaternion.LookRotation(Vector3.zero - myRigid.transform.position);
 					
 					myRigid.MoveRotation(Quaternion.RotateTowards(myRigid.transform.rotation, zeroDirection, turnSpeed));
-				}
+				}*/
 
-				//Roll around in local space
-				Spiral();
+				//Try it with targetProxy
+				Quaternion targetDirection = Quaternion.LookRotation (targetProxy.transform.position - myRigid.transform.position);
+					
+				myRigid.MoveRotation (Quaternion.RotateTowards (myRigid.transform.rotation, targetDirection, turnSpeed));
+
+
+			} 
+			else
+			if (!moving) 
+			{
+				//Move forward in a straight line
+				myRigid.velocity = transform.forward * movementVelocity;
 			}
+
+			float distanceToProxyPoint = Vector3.Distance(targetProxy.transform.position, myRigid.transform.position);
+
+			if (distanceToProxyPoint > closeRangeThreshold)
+			{
+				//only update the target pos if target is more than 10 meters away from missile
+				targetProxy.transform.position = target.transform.position;
+			}
+			else
+			if (distanceToProxyPoint < closeRangeThreshold)
+			{
+				//Turn off Movement
+				moving = false;
+				print("Free");
+			}
+
+			//Set the colour
+			if (distanceToProxyPoint > closeRangeThreshold + 10)
+			{
+				//Colour
+				status = Color.white;
+			}
+			else
+			if (distanceToProxyPoint < closeRangeThreshold + 10)
+			{
+				//Colour
+				status = Color.red;
+			}
+		
+			//targetProxy.transform.position = Vector3.Lerp(targetProxy.transform.position, target.transform.position, retargetSpeed* Time.deltaTime);
+
+			
+			//Roll around in local space
+			//Spiral();
 		}
 
 		void Fire()
 		{
+			//Reset angular velocity?
+			myRigid.angularVelocity = Vector3.zero;
+
+			print ("Fire");
+
 			if  (!moving)
 			{
-				//moving = true;
-				moving = !moving;
+				moving = true;
 			}
+
+			//Give the missile a target
+			targetProxy.transform.position = target.transform.position;
 		}
 
+
+		/*
 		void Spiral()
 		{
+
 			Vector3 pos = gameObject.transform.position;
+
 			Quaternion rot = Quaternion.AngleAxis (spiralSpeed * Time.time, Vector3.forward);
-			Vector3 dir = pos - new Vector3 (gameObject.transform.localPosition.x + 1, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
+			Vector3 dir = pos - new Vector3 (gameObject.transform.position.x+1, gameObject.transform.position.y, gameObject.transform.position.z);
 			dir = rot * dir;
-			
+
 			myRigid.MovePosition (pos + dir);
-		}
+		}*/
+
 	}
 }
