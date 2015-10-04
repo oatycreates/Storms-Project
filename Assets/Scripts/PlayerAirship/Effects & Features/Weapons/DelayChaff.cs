28b9b172cb2351dfc541c07340fa4809d770abe4
Pat Ferguson <patrick.ferguson@ancientdawngames.com>
2015-10-04 23:21:52 +1100

    Changed: Fixed chaff rigidbody slowing target finding code.

--- Assets/Scripts/PlayerAirship/Effects & Features/Weapons/DelayChaff.cs
+++ Assets/Scripts/PlayerAirship/Effects & Features/Weapons/DelayChaff.cs
@@ -20,95 +20,28 @@ namespace ProjectStorms
 	public class DelayChaff : MonoBehaviour 
 	{
 		private BoxCollider myCollider;
-		
-		Rigidbody[] m_playerRigidBodies = new Rigidbody[4];
-
-		void GetPlayerRigidBodies()
-		{
-			// Airships are the only things that will have AirshipControlBehaviours, this will only change if we refactor the input system
-			AirshipControlBehaviour[] objs = GameObject.FindObjectsOfType<AirshipControlBehaviour>();
-			// Use the script to find the rigidbody
-			StoreBodyInSlot(objs[0].GetComponent<Rigidbody>());
-			StoreBodyInSlot(objs[1].GetComponent<Rigidbody>());
-			StoreBodyInSlot(objs[2].GetComponent<Rigidbody>());
-			StoreBodyInSlot(objs[3].GetComponent<Rigidbody>());
-		}
-		
-		private void StoreBodyInSlot(Rigidbody a_body)
-		{
-			// Store in the slot
-			if (a_body != null)
-			{
-				if (a_body.CompareTag("Player1_"))
-				{
-					m_playerRigidBodies[0] = a_body;
-				}
-				else if (a_body.CompareTag("Player2_"))
-				{
-					m_playerRigidBodies[1] = a_body;
-				}
-				else if (a_body.CompareTag("Player3_"))
-				{
-					m_playerRigidBodies[2] = a_body;
-				}
-				else if (a_body.CompareTag("Player4_"))
-				{
-					m_playerRigidBodies[3] = a_body;
-				}
-			}
-		}
-
 
 		public void Awake() 
 		{
-			myCollider = gameObject.GetComponent<BoxCollider> ();
+			myCollider = gameObject.GetComponent<BoxCollider>();
 			myCollider.isTrigger = true;
-
-			GetPlayerRigidBodies ();
 		}
 
 		public void OnTriggerStay(Collider a_other)
 		{
-			Rigidbody playerBody = null;
-			
-			if (m_playerRigidBodies.Length > 0)
-			{
-				bool isPlayer = false;
-				switch (a_other.tag)
-				{
-				case "Player1_":
-					playerBody = m_playerRigidBodies[0];
-					isPlayer = true;
-					break;
-				case "Player2_":
-					playerBody = m_playerRigidBodies[1];
-					isPlayer = true;
-					break;
-				case "Player3_":
-					playerBody = m_playerRigidBodies[2];
-					isPlayer = true;
-					break;
-				case "Player4_":
-					playerBody = m_playerRigidBodies[3];
-					isPlayer = true;
-					break;
-				case "Passengers":
-					playerBody = a_other.GetComponentInParent<Rigidbody>();
-					break;
-				default:
-					// Not player
-					playerBody = a_other.GetComponent<Rigidbody>();
-					return;
-				}
-				
-				// Ensure that player has a rigidbody
-				if (playerBody != null)
-				{
-					//This seems to be the best value for the time being- adjust this as necessary.
-					playerBody.velocity *= 0.985f;
-				}
-			}
-		}
+            Rigidbody rigid = a_other.GetComponent<Rigidbody>();
 
+            if (rigid == null)
+            {
+                rigid = a_other.GetComponentInParent<Rigidbody>();
+            }
+
+            // Ensure that player has a rigidbody
+            if (rigid != null)
+            {
+                //This seems to be the best value for the time being- adjust this as necessary.
+                rigid.velocity *= 0.985f;
+            }
+		}
 	}
 }
