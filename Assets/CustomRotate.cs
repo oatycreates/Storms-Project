53aa7cf1d07dd1dabc4ae92e9938f41d69c89069
BrokenProxy <nerdrowan@gmail.com>
2015-10-09 13:47:34 +1100

    Changed: Minor changes to splash screen scene.

---
+++ Assets/CustomRotate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomRotate : MonoBehaviour 
+{
+	void Update () 
+    {
+        gameObject.transform.Rotate(Vector3.forward * -80* Time.deltaTime, Space.Self);
+	}
+}
