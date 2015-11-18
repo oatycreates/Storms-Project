a54741dd44781a4dca4132754c33396a52ffe93c
Rowan Donaldson <nerdrowan@gmail.com>
2015-11-19 02:48:45 +1100

    Changed: Cogs in the menu scene.

---
+++ Assets/CogSpriteRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CogSpriteRotate : MonoBehaviour 
+{
+	public float rotateSpeed = 1;
+
+	void Update()
+	{
+		gameObject.transform.Rotate( Vector3.forward * rotateSpeed, Space.Self);	
+	}
+}
