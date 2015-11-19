c8f6e614a382159272a2932c433e9a4141c4d951
Andrew Barbour <andrew.barbour1@gmail.com>
2015-11-19 11:34:58 +1100

    Added: Simple rotator script for rotating objects

---
+++ Assets/Scripts/Utilities/Rotator.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * File: Rotator.cs
+ * Author: Andrew Barbour
+ * Maintainer: Andrew Barbour
+ * Created: 19/11/2015
+ * Copyright: (c) 2015 Team Storms, All Rights Reserved.
+ * Description: Rotates an object
+ **/
+
+using UnityEngine;
+using System.Collections;
+
+namespace StormsProject
+{
+    public class Rotator : MonoBehaviour
+    {
+        public Vector3 rotationAxis;
+        public float speed;
+
+        Transform _transform;
+
+        public void Awake()
+        {
+            _transform = transform;
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            _transform.Rotate(rotationAxis * speed * Time.deltaTime);
+        }
+    }
+}
